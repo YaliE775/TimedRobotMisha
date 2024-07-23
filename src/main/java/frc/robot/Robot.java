1c75ebe46936405cbef87191c7071ba46ac9c611
SycaTech <yalienchoa@gmail.com>
2024-07-23 20:29:26 +0300

    feat(): add talon fx pid

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,7 +4,12 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.controls.PositionDutyCycle;
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -13,42 +18,69 @@ import edu.wpi.first.wpilibj.TimedRobot;
  * project.
  */
 public class Robot extends TimedRobot {
-  
-  @Override
-  public void robotInit() {
-    
-  }
+    private TalonFX motor;
 
-  @Override
-  public void robotPeriodic() {}
+    private double setPoint;
+    private double kP;
+    private double kI;
+    private double kD;
 
-  @Override
-  public void autonomousInit() {}
+    @Override
+    public void robotInit() {
+        motor = new TalonFX(5);
+        SmartDashboard.putNumber("kP", 0);
+        SmartDashboard.putNumber("kI", 0);
+        SmartDashboard.putNumber("kD", 0);
+        SmartDashboard.putNumber("Set Point", setPoint);
 
-  @Override
-  public void autonomousPeriodic() {}
+    }
 
-  @Override
-  public void teleopInit() {}
+    @Override
+    public void robotPeriodic() {
+        setPoint = SmartDashboard.getNumber("Set Point", 0);
 
-  @Override
-  public void teleopPeriodic() {}
+        kP = SmartDashboard.getNumber("kP", 0);
+        kI = SmartDashboard.getNumber("kI", 0);
+        kD = SmartDashboard.getNumber("kD", 0);
 
-  @Override
-  public void disabledInit() {}
+        Slot0Configs configs = new Slot0Configs()
+            .withKP(kP)
+            .withKI(kI)
+            .withKD(kD);
 
-  @Override
-  public void disabledPeriodic() {}
+        motor.getConfigurator().apply(configs);
 
-  @Override
-  public void testInit() {}
+    }
 
-  @Override
-  public void testPeriodic() {}
+    @Override
+    public void autonomousInit() {}
 
-  @Override
-  public void simulationInit() {}
+    @Override
+    public void autonomousPeriodic() {}
 
-  @Override
-  public void simulationPeriodic() {}
+    @Override
+    public void teleopInit() {}
+
+    @Override
+    public void teleopPeriodic() {
+        motor.setControl(new PositionDutyCycle(setPoint));
+    }
+
+    @Override
+    public void disabledInit() {}
+
+    @Override
+    public void disabledPeriodic() {}
+
+    @Override
+    public void testInit() {}
+
+    @Override
+    public void testPeriodic() {}
+
+    @Override
+    public void simulationInit() {}
+
+    @Override
+    public void simulationPeriodic() {}
 }
